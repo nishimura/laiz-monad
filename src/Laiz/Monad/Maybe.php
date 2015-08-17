718e1ea26b6162671eb63a13235a77d133bb0601
Satoshi Nishimura <nishim314@gmail.com>
2015-08-17 16:16:38 +0900

    add abstract method in Maybe

--- src/Laiz/Monad/Maybe.php
+++ src/Laiz/Monad/Maybe.php
@@ -6,4 +6,16 @@ abstract class Maybe implements Monad, MonadPlus
 {
     use MonadTrait;
     use MaybeTrait;
+
+    /**
+     * @param $a mixed
+     * @return mixed
+     */
+    abstract public function fromMaybe($a);
+
+    /**
+     * @param $a mixed
+     * @return mixed
+     */
+    abstract public function maybe($a, callable $f);
 }
