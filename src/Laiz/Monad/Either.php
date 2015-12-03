e9929594f331b32f3d58523e2049538de0a37d18
Satoshi Nishimura <nishim314@gmail.com>
2015-12-03 17:18:40 +0900

    remove context files

--- src/Laiz/Monad/Either.php
+++ src/Laiz/Monad/Either.php
@@ -5,7 +5,6 @@ namespace Laiz\Monad;
 abstract class Either implements Monad
 {
     use MonadTrait;
-    use EitherTrait;
 
     /**
      * @param mixed
@@ -14,4 +13,21 @@ abstract class Either implements Monad
     {
         $this->value = $value;
     }
+
+    public static function ret($a)
+    {
+        if ($a === null)
+            throw new \InvalidArgumentException('null is invalid');
+
+        return new Either\Right($a);
+    }
+
+    /**
+     * @param $a string
+     */
+    public static function fail($a = null)
+    {
+        return new Either\Left($a);
+    }
+
 }
--- src/Laiz/Monad/MonadList.php
+++ src/Laiz/Monad/MonadList.php
@@ -5,7 +5,24 @@ namespace Laiz\Monad;
 abstract class MonadList implements Monad, MonadPlus, \IteratorAggregate, \Countable
 {
     use MonadTrait;
-    use MonadListTrait;
+
+    public static function ret($a)
+    {
+        if ($a === null)
+            throw new \InvalidArgumentException('null is invalid');
+
+        return MonadList::cast(new \ArrayIterator([$a]));
+    }
+
+    public static function fail($a = null)
+    {
+        return MonadList\Nil::getInstance();
+    }
+
+    public static function mzero()
+    {
+        return self::fail();
+    }
 
     /**
      * @return Laiz\Monad\DataList
